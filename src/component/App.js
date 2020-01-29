9e4b7da50d4f030e42c0174d8c92c15b34bdd619
psyak14 <psyak14@nottingham.ac.uk>
2020-01-29 16:01:34 +0000

    Set up router

--- src/component/App.js
+++ src/component/App.js
@@ -2,52 +2,23 @@ import React from "react";
 import {
   BrowserRouter as Router,
   Switch,
-  Route,
-  Link
+  Route
 } from "react-router-dom";
+
+import Festivals from './Festivals'
+import FestivalAwards from './FestivalAwards'
 
 export default function App() {
   return (
     <Router>
-      <div>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/about">About</Link>
-            </li>
-            <li>
-              <Link to="/users">Users</Link>
-            </li>
-          </ul>
-        </nav>
-
         <Switch>
-          <Route path="/about">
-            <About />
-          </Route>
-          <Route path="/users">
-            <Users />
-          </Route>
           <Route path="/">
-            <Home />
+            <Festivals />
+          </Route>
+          <Route path="/festival/:id">
+            <FestivalAwards />
           </Route>
         </Switch>
-      </div>
     </Router>
   );
-}
-
-function Home() {
-  return <h2>Home</h2>;
-}
-
-function About() {
-  return <h2>About</h2>;
-}
-
-function Users() {
-  return <h2>Users</h2>;
 }
---
+++ src/component/FestivalAwards.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function FestivalAwards() {
+	return <h1>This is the list of all awards for this festival</h1>
+}
