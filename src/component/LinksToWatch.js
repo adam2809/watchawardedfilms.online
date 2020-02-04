08ae177cc4c4ba2d17c3c229ab29f1502f5881ef
psyak14 <psyak14@nottingham.ac.uk>
2020-02-04 14:31:43 +0000

    Wrote basic version of WatchModal

---
+++ src/component/LinksToWatch.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function LinksToWatch(props){
+	return (
+		<div>
+			<h3>{props.monetizationType}</h3>
+			{props.offers.filter(o => o.monetization_type == props.monetizationType)
+                         .map(o => <p><a target="_blank" rel="noopener noreferrer" href={o.urls.standard_web}>{o.urls.standard_web}</a><br/>{o.retail_price}</p>)
+	 		}
+		</div>
+	)
+}
