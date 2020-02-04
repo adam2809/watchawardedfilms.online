31275f5732782bd26fbb2baae982ec97fe6fb908
psyak14 <psyak14@nottingham.ac.uk>
2020-02-04 15:22:55 +0000

    Now WatchModal is calling test api instead of hardcoded test data

--- src/component/LinksToWatch.js
+++ src/component/LinksToWatch.js
@@ -5,7 +5,17 @@ export default function LinksToWatch(props){
 		<div>
 			<h3>{props.monetizationType}</h3>
 			{props.offers.filter(o => o.monetization_type == props.monetizationType)
-                         .map(o => <p><a target="_blank" rel="noopener noreferrer" href={o.urls.standard_web}>{o.urls.standard_web}</a><br/>{o.retail_price}</p>)
+                         .map((o,i) => {
+							 return (
+								<p key={i}>
+								 	<a target="_blank" rel="noopener noreferrer" href={o.urls.standard_web}>
+										{o.urls.standard_web}
+									</a>
+									<br/>
+									{o.retail_price}
+								</p>
+							 )
+						 })
 	 		}
 		</div>
 	)
--- src/component/WatchModal.js
+++ src/component/WatchModal.js
@@ -3,366 +3,51 @@ import LinksToWatch from './LinksToWatch'
 
 export default class WatchModal extends React.Component {
 
-	response = {
-	    "total_results": 18329,
-	    "items": [
-	        {
-	            "jw_entity_id": "tm120702",
-	            "id": 120702,
-	            "title": "Belle de Jour",
-	            "full_path": "/uk/movie/belle-de-jour",
-	            "full_paths": {
-	                "MOVIE_DETAIL_OVERVIEW": "/uk/movie/belle-de-jour"
-	            },
-	            "poster": "/poster/8822270/{profile}",
-	            "original_release_year": 1967,
-	            "tmdb_popularity": 10.239,
-	            "object_type": "movie",
-	            "localized_release_date": "2017-09-08",
-	            "offers": [
-	                {
-	                    "monetization_type": "buy",
-	                    "provider_id": 10,
-	                    "retail_price": 4.99,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://www.amazon.co.uk/gp/product/B00ET1V14K?camp=1634&creativeASIN=B00ET1V14K&ie=UTF8&linkCode=xm2&tag=just016-21",
-	                        "deeplink_android_tv": "intent:#Intent;component=com.amazon.amazonvideo.livingroom/com.amazon.ignition.IgnitionActivity;S.com.amazon.ignition.DeepLinkIntent.DEEP_LINK=B00ET1V14K;end",
-	                        "deeplink_fire_tv": "intent://com.amazon.tv.launcher/detail?provider=aiv&providerId=B00ET1V14K#Intent;package=com.amazon.tv.launcher;scheme=amzn;end",
-	                        "deeplink_tvos": "aiv://aiv/watch?asin=B00ET1V14K&territory=GB&time=auto"
-	                    },
-	                    "presentation_type": "sd",
-	                    "date_provider_id": "2016-11-26_10",
-	                    "date_created": "2016-11-26"
-	                },
-	                {
-	                    "monetization_type": "rent",
-	                    "provider_id": 10,
-	                    "retail_price": 3.49,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://www.amazon.co.uk/gp/product/B00ET1V14K?camp=1634&creativeASIN=B00ET1V14K&ie=UTF8&linkCode=xm2&tag=just016-21",
-	                        "deeplink_android_tv": "intent:#Intent;component=com.amazon.amazonvideo.livingroom/com.amazon.ignition.IgnitionActivity;S.com.amazon.ignition.DeepLinkIntent.DEEP_LINK=B00ET1V14K;end",
-	                        "deeplink_fire_tv": "intent://com.amazon.tv.launcher/detail?provider=aiv&providerId=B00ET1V14K#Intent;package=com.amazon.tv.launcher;scheme=amzn;end",
-	                        "deeplink_tvos": "aiv://aiv/watch?asin=B00ET1V14K&territory=GB&time=auto"
-	                    },
-	                    "presentation_type": "hd",
-	                    "date_provider_id": "2016-11-26_10",
-	                    "date_created": "2016-11-26"
-	                },
-	                {
-	                    "monetization_type": "rent",
-	                    "provider_id": 10,
-	                    "retail_price": 2.49,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://www.amazon.co.uk/gp/product/B00ET1V14K?camp=1634&creativeASIN=B00ET1V14K&ie=UTF8&linkCode=xm2&tag=just016-21",
-	                        "deeplink_android_tv": "intent:#Intent;component=com.amazon.amazonvideo.livingroom/com.amazon.ignition.IgnitionActivity;S.com.amazon.ignition.DeepLinkIntent.DEEP_LINK=B00ET1V14K;end",
-	                        "deeplink_fire_tv": "intent://com.amazon.tv.launcher/detail?provider=aiv&providerId=B00ET1V14K#Intent;package=com.amazon.tv.launcher;scheme=amzn;end",
-	                        "deeplink_tvos": "aiv://aiv/watch?asin=B00ET1V14K&territory=GB&time=auto"
-	                    },
-	                    "presentation_type": "sd",
-	                    "date_provider_id": "2016-11-26_10",
-	                    "date_created": "2016-11-26"
-	                },
-	                {
-	                    "monetization_type": "buy",
-	                    "provider_id": 10,
-	                    "retail_price": 5.99,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://www.amazon.co.uk/gp/product/B00ET1V14K?camp=1634&creativeASIN=B00ET1V14K&ie=UTF8&linkCode=xm2&tag=just016-21",
-	                        "deeplink_android_tv": "intent:#Intent;component=com.amazon.amazonvideo.livingroom/com.amazon.ignition.IgnitionActivity;S.com.amazon.ignition.DeepLinkIntent.DEEP_LINK=B00ET1V14K;end",
-	                        "deeplink_fire_tv": "intent://com.amazon.tv.launcher/detail?provider=aiv&providerId=B00ET1V14K#Intent;package=com.amazon.tv.launcher;scheme=amzn;end",
-	                        "deeplink_tvos": "aiv://aiv/watch?asin=B00ET1V14K&territory=GB&time=auto"
-	                    },
-	                    "presentation_type": "hd",
-	                    "date_provider_id": "2016-11-26_10",
-	                    "date_created": "2016-11-26"
-	                },
-	                {
-	                    "monetization_type": "buy",
-	                    "provider_id": 130,
-	                    "retail_price": 5.99,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://www.skystore.com/product/belle-de-jour/8ae6389c-a7bc-40ec-9ca4-248a0f2d9e80"
-	                    },
-	                    "presentation_type": "hd",
-	                    "date_provider_id": "2017-10-08_130",
-	                    "date_created": "2017-10-08"
-	                },
-	                {
-	                    "monetization_type": "rent",
-	                    "provider_id": 2,
-	                    "retail_price": 3.49,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://itunes.apple.com/gb/movie/belle-de-jour-1967/id1292422614?uo=4&at=1000l3V2"
-	                    },
-	                    "presentation_type": "sd",
-	                    "date_provider_id": "2017-10-21_2",
-	                    "date_created": "2017-10-21"
-	                },
-	                {
-	                    "monetization_type": "buy",
-	                    "provider_id": 2,
-	                    "retail_price": 5.99,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://itunes.apple.com/gb/movie/belle-de-jour-1967/id1292422614?uo=4&at=1000l3V2"
-	                    },
-	                    "presentation_type": "hd",
-	                    "date_provider_id": "2017-10-21_2",
-	                    "date_created": "2017-10-21"
-	                },
-	                {
-	                    "monetization_type": "buy",
-	                    "provider_id": 2,
-	                    "retail_price": 4.99,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://itunes.apple.com/gb/movie/belle-de-jour-1967/id1292422614?uo=4&at=1000l3V2"
-	                    },
-	                    "presentation_type": "sd",
-	                    "date_provider_id": "2017-10-21_2",
-	                    "date_created": "2017-10-21"
-	                },
-	                {
-	                    "monetization_type": "rent",
-	                    "provider_id": 2,
-	                    "retail_price": 3.49,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://itunes.apple.com/gb/movie/belle-de-jour-1967/id1292422614?uo=4&at=1000l3V2"
-	                    },
-	                    "presentation_type": "hd",
-	                    "date_provider_id": "2017-10-21_2",
-	                    "date_created": "2017-10-21"
-	                },
-	                {
-	                    "monetization_type": "buy",
-	                    "provider_id": 130,
-	                    "retail_price": 7.99,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://www.skystore.com/product/belle-de-jour/8ae6389c-a7bc-40ec-9ca4-248a0f2d9e80"
-	                    },
-	                    "presentation_type": "dvd",
-	                    "date_provider_id": "2018-06-08_130",
-	                    "date_created": "2018-06-08"
-	                },
-	                {
-	                    "monetization_type": "buy",
-	                    "provider_id": 3,
-	                    "retail_price": 7.99,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://play.google.com/store/movies/details/Belle_de_Jour?gl=GB&hl=en&id=TA9JR3yg16g",
-	                        "deeplink_android_tv": "TA9JR3yg16g"
-	                    },
-	                    "audio_languages": [
-	                        "fr"
-	                    ],
-	                    "presentation_type": "hd",
-	                    "date_provider_id": "2018-09-03_3",
-	                    "date_created": "2018-09-03"
-	                },
-	                {
-	                    "monetization_type": "buy",
-	                    "provider_id": 3,
-	                    "retail_price": 5.99,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://play.google.com/store/movies/details/Belle_de_Jour?gl=GB&hl=en&id=TA9JR3yg16g",
-	                        "deeplink_android_tv": "TA9JR3yg16g"
-	                    },
-	                    "audio_languages": [
-	                        "fr"
-	                    ],
-	                    "presentation_type": "sd",
-	                    "date_provider_id": "2018-09-03_3",
-	                    "date_created": "2018-09-03"
-	                },
-	                {
-	                    "monetization_type": "rent",
-	                    "provider_id": 3,
-	                    "retail_price": 3.49,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://play.google.com/store/movies/details/Belle_de_Jour?gl=GB&hl=en&id=TA9JR3yg16g",
-	                        "deeplink_android_tv": "TA9JR3yg16g"
-	                    },
-	                    "audio_languages": [
-	                        "fr"
-	                    ],
-	                    "presentation_type": "hd",
-	                    "date_provider_id": "2018-09-03_3",
-	                    "date_created": "2018-09-03"
-	                },
-	                {
-	                    "monetization_type": "rent",
-	                    "provider_id": 3,
-	                    "retail_price": 2.49,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://play.google.com/store/movies/details/Belle_de_Jour?gl=GB&hl=en&id=TA9JR3yg16g",
-	                        "deeplink_android_tv": "TA9JR3yg16g"
-	                    },
-	                    "audio_languages": [
-	                        "fr"
-	                    ],
-	                    "presentation_type": "sd",
-	                    "date_provider_id": "2018-09-03_3",
-	                    "date_created": "2018-09-03"
-	                },
-	                {
-	                    "monetization_type": "rent",
-	                    "provider_id": 192,
-	                    "retail_price": 3.49,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://www.youtube.com/watch?v=TA9JR3yg16g",
-	                        "deeplink_android_tv": "TA9JR3yg16g"
-	                    },
-	                    "audio_languages": [
-	                        "fr"
-	                    ],
-	                    "presentation_type": "hd",
-	                    "date_provider_id": "2018-11-16_192",
-	                    "date_created": "2018-11-16"
-	                },
-	                {
-	                    "monetization_type": "rent",
-	                    "provider_id": 192,
-	                    "retail_price": 2.49,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://www.youtube.com/watch?v=TA9JR3yg16g",
-	                        "deeplink_android_tv": "TA9JR3yg16g"
-	                    },
-	                    "audio_languages": [
-	                        "fr"
-	                    ],
-	                    "presentation_type": "sd",
-	                    "date_provider_id": "2018-11-16_192",
-	                    "date_created": "2018-11-16"
-	                },
-	                {
-	                    "monetization_type": "buy",
-	                    "provider_id": 192,
-	                    "retail_price": 7.99,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://www.youtube.com/watch?v=TA9JR3yg16g",
-	                        "deeplink_android_tv": "TA9JR3yg16g"
-	                    },
-	                    "audio_languages": [
-	                        "fr"
-	                    ],
-	                    "presentation_type": "hd",
-	                    "date_provider_id": "2018-11-16_192",
-	                    "date_created": "2018-11-16"
-	                },
-	                {
-	                    "monetization_type": "buy",
-	                    "provider_id": 192,
-	                    "retail_price": 5.99,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://www.youtube.com/watch?v=TA9JR3yg16g",
-	                        "deeplink_android_tv": "TA9JR3yg16g"
-	                    },
-	                    "audio_languages": [
-	                        "fr"
-	                    ],
-	                    "presentation_type": "sd",
-	                    "date_provider_id": "2018-11-16_192",
-	                    "date_created": "2018-11-16"
-	                },
-	                {
-	                    "monetization_type": "flatrate",
-	                    "provider_id": 9,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://www.amazon.co.uk/gp/product/B00ET1V14K?camp=1634&creativeASIN=B00ET1V14K&ie=UTF8&linkCode=xm2&tag=just016-21",
-	                        "deeplink_android_tv": "intent:#Intent;component=com.amazon.amazonvideo.livingroom/com.amazon.ignition.IgnitionActivity;S.com.amazon.ignition.DeepLinkIntent.DEEP_LINK=B00ET1V14K;end",
-	                        "deeplink_fire_tv": "intent://com.amazon.tv.launcher/detail?provider=aiv&providerId=B00ET1V14K#Intent;package=com.amazon.tv.launcher;scheme=amzn;end",
-	                        "deeplink_tvos": "aiv://aiv/watch?asin=B00ET1V14K&territory=GB&time=auto"
-	                    },
-	                    "presentation_type": "hd",
-	                    "date_provider_id": "2019-02-08_9",
-	                    "date_created": "2019-02-08"
-	                },
-	                {
-	                    "monetization_type": "flatrate",
-	                    "provider_id": 9,
-	                    "currency": "GBP",
-	                    "urls": {
-	                        "standard_web": "https://www.amazon.co.uk/gp/product/B00ET1V14K?camp=1634&creativeASIN=B00ET1V14K&ie=UTF8&linkCode=xm2&tag=just016-21",
-	                        "deeplink_android_tv": "intent:#Intent;component=com.amazon.amazonvideo.livingroom/com.amazon.ignition.IgnitionActivity;S.com.amazon.ignition.DeepLinkIntent.DEEP_LINK=B00ET1V14K;end",
-	                        "deeplink_fire_tv": "intent://com.amazon.tv.launcher/detail?provider=aiv&providerId=B00ET1V14K#Intent;package=com.amazon.tv.launcher;scheme=amzn;end",
-	                        "deeplink_tvos": "aiv://aiv/watch?asin=B00ET1V14K&territory=GB&time=auto"
-	                    },
-	                    "presentation_type": "sd",
-	                    "date_provider_id": "2019-02-08_9",
-	                    "date_created": "2019-02-08"
-	                }
-	            ],
-	            "scoring": [
-	                {
-	                    "provider_type": "tmdb:score",
-	                    "value": 7.5
-	                },
-	                {
-	                    "provider_type": "tmdb:id",
-	                    "value": 649
-	                },
-	                {
-	                    "provider_type": "imdb:score",
-	                    "value": 7.7
-	                },
-	                {
-	                    "provider_type": "tomato:meter",
-	                    "value": 96
-	                },
-	                {
-	                    "provider_type": "tomato_userrating:meter",
-	                    "value": 86
-	                },
-	                {
-	                    "provider_type": "tmdb:popularity",
-	                    "value": 10.239
-	                },
-	                {
-	                    "provider_type": "tomato:id",
-	                    "value": 770689374
-	                },
-	                {
-	                    "provider_type": "tomato:rating",
-	                    "value": 1
-	                }
-	            ],
-	            "cinema_release_date": "2017-09-08"
-	        }]}
-
 	constructor(props){
 		super(props)
+		this.state = {
+			isLoading: true,
+			errorOccurred: false,
+			jwResponse: {}
+		}
 	}
 
 	componentDidMount(){
+		fetch(process.env.REACT_APP_API_URL+'test')
+		.then(res => res.json())
+		.then(res => {
+			this.setState({
+				isLoading:false,
+				jwResponse:res
+			})
+		})
+		.catch(err => {
+			this.setState({
+				isLoading:false,
+				errorOcurred:true
+			})
+		})
 	}
 
 	getMonetizationTypes(){
-		return [...new Set(this.response.items[0].offers.map(o => o.monetization_type))]
+		if(this.state.isLoading || this.state.errorOcurred){
+			return []
+		}
+		return [...new Set(this.state.jwResponse.items[0].offers.map(o => o.monetization_type))]
 	}
 
 	render(){
 		return (
 			<div>
 				<h2>Where to watch {this.props.title}</h2>
-				{this.getMonetizationTypes().map(monetType => {
-					return <LinksToWatch monetizationType={monetType} offers={this.response.items[0].offers}/>
+				{this.state.isLoading && <h3>Loading...</h3>}
+				{this.state.errorOcurred && <h3>Could not retrieve films list</h3>}
+				{this.getMonetizationTypes().map((monetType,i) => {
+					return <LinksToWatch
+								monetizationType={monetType}
+								offers={this.state.jwResponse.items[0].offers}
+								key={i}
+							/>
 				})}
 			</div>
 		)
