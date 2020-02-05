babf33523546c33ce75365f19a23bf29ab6e3a44
psyak14 <psyak14@nottingham.ac.uk>
2020-02-05 14:28:22 +0000

    Added Loader to FestivalAwards

--- src/component/FestivalAwards.js
+++ src/component/FestivalAwards.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { withRouter } from "react-router-dom";
 
-import { makeStyles } from '@material-ui/core/styles';
 import Table from '@material-ui/core/Table';
 import TableBody from '@material-ui/core/TableBody';
 import TableCell from '@material-ui/core/TableCell';
@@ -10,6 +9,7 @@ import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
 import Box from '@material-ui/core/Box';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 class FestivalAwards extends React.Component {
 	fId
@@ -44,30 +44,33 @@ class FestivalAwards extends React.Component {
 			<div align='center'>
 				<h1>This is the list of all awards for {this.props.getFestivals()[this.fId-1]}</h1>
 				<Box width={600}>
-					<TableContainer component={Paper}>
-						<Table aria-label={"Awards for "+this.props.getFestivals()[this.fId-1]}>
-							<TableHead>
-								<TableRow>
-									<TableCell>Title</TableCell>
-									<TableCell>Award</TableCell>
-									<TableCell>Year</TableCell>
-								</TableRow>
-							</TableHead>
-							<TableBody>
-								{this.state.films.map((f,i) => (
-									<TableRow key={i}>
-										<TableCell>{f.name}</TableCell>
-										<TableCell>{f.award}</TableCell>
-										<TableCell>{f.year}</TableCell>
+				{this.state.isLoading ? <CircularProgress color='primary' /> :
+						<TableContainer component={Paper}>
+							<Table aria-label={"Awards for "+this.props.getFestivals()[this.fId-1]}>
+								<TableHead>
+									<TableRow>
+										<TableCell>Title</TableCell>
+										<TableCell>Award</TableCell>
+										<TableCell>Year</TableCell>
 									</TableRow>
-								))}
-							</TableBody>
-						</Table>
-					</TableContainer>
+								</TableHead>
+								<TableBody>
+									{
+										this.state.films.sort((a,b) => (a.year < b.year) ? 1 : -1).map((f,i) => (
+											<TableRow key={i}>
+												<TableCell>{f.name}</TableCell>
+												<TableCell>{f.award}</TableCell>
+												<TableCell>{f.year}</TableCell>
+											</TableRow>
+										))
+									}
+								</TableBody>
+							</Table>
+						</TableContainer>
+					}
 				</Box>
 
 
-				{this.state.isLoading && <h2>Loading...</h2>}
 				{this.state.errorOcurred && <h2>Could not retrieve films list</h2>}
 
 				{this.state.films.map((f,i) => <p key={i}>{i}: {f.name} | {f.year} | {f.award}</p>)}
