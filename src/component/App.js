d2fca30fb2a14e2a522d3b0c320b2b36b95ce4a7
adam2809 <adam.kule@gmail.com>
2020-04-02 16:29:33 +0100

    Added basic app bar

--- src/component/App.js
+++ src/component/App.js
@@ -9,6 +9,13 @@ import Festivals from './Festivals'
 import FestivalAwards from './FestivalAwards'
 import WatchDialog from './WatchDialog'
 
+import AppBar from '@material-ui/core/AppBar';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
+
 let festivals = []
 
 class App extends React.Component{
@@ -38,6 +45,16 @@ class App extends React.Component{
 
     render(){
         return (
+            <>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6">
+                        watchawardedfilms.online
+                    </Typography>
+                    <Button color="inherit">Festivals</Button>
+                    <Button color="inherit">About</Button>
+                </Toolbar>
+            </AppBar>
             <Router>
                 <Switch>
                     <Route path="/test">
@@ -55,6 +72,7 @@ class App extends React.Component{
                     </Route>
                 </Switch>
             </Router>
+            </>
         );
     }
 }
